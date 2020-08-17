279594efdfe9046290d3d22a93cf0c0b7f9d7620
Shubham Badgujar <shubhambadgujar222@gmail.com>
2020-08-17 11:30:51 +0530

    refreash for rpi

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const launchConfig = {
     autoRefresh:true,
     headless: true,
     cacheEnabled:true,
+    executablePath: "/usr/bin/chromium-browser",
     customUserAgent:"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
 };
 wa.create(launchConfig).then(client => start(client));
